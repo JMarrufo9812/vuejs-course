bc0248eb9be3f8fbc4bd1e7132d1428d7b21f732
eliot marrufo <eliot@cervusconsuting.com>
2020-02-05 13:34:25 -0600

    d

--- src/components/Buttom.vue
+++ src/components/Buttom.vue
@@ -1,22 +1,27 @@
 <template>
-    <v-btn fab dark color="indigo">
-      <v-icon dark>mdi-plus</v-icon>
-    </v-btn>
-  <!-- <ModalAddContactsForm/> -->
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn class="mx-2" v-on="on" fab dark color="indigo">
+          <v-icon dark>mdi-plus</v-icon>
+        </v-btn>
+      </template>
+      <ModalAddContactForm :dialog="dialog" />
+    </v-dialog>
+  </v-row>
 </template>
 
+
 <script>
-// import ModalAddContactForm from "../components/ModalAddContactForm";
+import ModalAddContactForm from "./ModalAddContactForm";
 
 export default {
-  data() {
-    return {
-      dialog: false
-    };
+  data: () => ({
+    dialog: false
+  }),
+  components: {
+    ModalAddContactForm
   }
-  // components: {
-  //   ModalAddContactForm
-  // }
 };
 </script>
 
